f68e31d03d3ee015107e5a597632506cdff41741
Arun <96939883+RV-Arun22@users.noreply.github.com>
2025-07-05 16:32:52 +0530

    Update synchr_FIFO.v
    Had problems operating when full or empty were 1. Was due to having independent count and operation updation.

--- 10. Synchronous FIFO/synchr_FIFO.v
+++ 10. Synchronous FIFO/synchr_FIFO.v
@@ -22,8 +22,7 @@
 //                      'count' updation also uses a separate always block.
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module synchr_FIFO #(parameter width = 8, depth = 8)(
+module synchr_FIFO#(parameter width = 8, depth = 8)(
     input clk, rst,
     input [(width-1):0] data_in,
     input wen, ren,                 //write and read enable signals
@@ -38,45 +37,42 @@ module synchr_FIFO #(parameter width = 8, depth = 8)(
     assign full = (count == depth);
     assign empty = (count == 0);
     
-    always @ (posedge clk)          //write always block
-        begin   
+    always @ (posedge clk)
+        begin
             if (rst ==1'b1)
                 begin
                     wptr <= 'b0;
-                    count <= 0;
-                end
-            else if (wen && !full)
-                begin
-                    fifo[wptr] <= data_in;
-                    wptr <= wptr + 1'b1;        //count not updated here as a simultaneous read could occur
-                end
-        end
-                
-    always @ (posedge clk)      //read always block
-        begin   
-            if (rst ==1'b1)
-                begin
                     rptr <= 'b0;
                     count <= 0;
                 end
-            else if (ren && !empty)
-                begin
-                    data_out <= fifo[rptr];
-                    rptr <= rptr + 1'b1;        //count not updated here as a simultaneous write could occur
-                end
-        end
-        
-    always @ (posedge clk)
-        begin
-            if (rst == 1'b1)
-                count <= 0;
-            else 
+            else
                 begin
                     case ({wen, ren})
-                        2'b01: count <= count - 1'b1;       //data is read, count decreases
-                        2'b10: count <= count + 1'b1;       //data is written, count increases
-                        default: count <= count;            //no update of count if 2'b11 and 2'b00
-                    endcase
+                        2'b01: begin
+                                   if(!empty)
+                                       begin
+                                           data_out <= fifo[rptr];
+                                           rptr <= rptr + 1'b1;        
+                                           count <= count - 1;
+                                       end
+                               end 
+                        2'b10: begin
+                                   if(!full)
+                                       begin
+                                           fifo[wptr] <= data_in;
+                                           wptr <= wptr + 1'b1;
+                                           count <= count + 1;
+                                       end
+                               end
+                        2'b11: begin
+                                   fifo[wptr] <= data_in;
+                                   wptr <= wptr + 1'b1;
+                                   data_out <= fifo[rptr];
+                                   rptr <= rptr + 1'b1;
+                                   count <= count;  
+                               end
+                        2'b00: count <= count;
+                    endcase    
                 end
-        end        
+        end
 endmodule
